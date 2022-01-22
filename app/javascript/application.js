60cdd2c93e3d2270e395fc14f0820bc76f1ac240
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-01-22 20:36:07 +0900

    scaffold

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "components/hello"
+
+const container = document.querySelector("#hello");
+if (container) {
+  ReactDOM.render(
+    React.createElement(Hello, { toWhat: container.dataset.toWhat }, null),
+    container
+  );
+}
